211508f82907344d59d5d790112f5f0c391e6faa
X-Yin <34438874+X-Yin@users.noreply.github.com>
2019-08-04 21:02:37 +0800

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.end('hello world!');
+    res.end('hello world!123');
 }).listen(8888, () => {
     console.log('server running at http://localhost:8888');
 });
